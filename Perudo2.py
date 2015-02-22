5780f85abb9052dfd6badebce76693238029b6e9
Arnaud Fraulaud <arnaud@fraulaud.fr>
2015-02-22 09:27:53 +0100

    server and chan un-hardcoded

--- Perudo2.py
+++ Perudo2.py
@@ -5,9 +5,12 @@ import irclib
 import ircbot
 import random
 import re
+import sys
 
 class PeruBot(ircbot.SingleServerIRCBot):
 
+        server = sys.argv[1]
+        chan = '#'+sys.argv[2]
 	state = 'PRE-JEU'
 	players = {}
 	order = []
@@ -17,16 +20,16 @@ class PeruBot(ircbot.SingleServerIRCBot):
 	palifico = False
 
 	def __init__(self):
-		ircbot.SingleServerIRCBot.__init__(self, [("irc.rezel.org", 6667)],
+		ircbot.SingleServerIRCBot.__init__(self, [(self.server, 6667)],
                                            "PeruBot", "Bot pour jouer au Perudo écrit par traklon en Python à l'aide du module ircbot.")
 
 	def on_welcome(self, serv, ev):
-		serv.join("#perudo")
+		serv.join(self.chan)
 
 	def on_join(self, serv, ev):
 		author = irclib.nm_to_n(ev.source())
 		if self.state == 'PRE-JEU':
-			serv.privmsg("#perudo", "Bienvenue " + author + " ! Tape !join pour rejoindre ! Rappel : tapez !play pour lancer la partie !")
+			serv.privmsg(self.chan, "Bienvenue " + author + " ! Tape !join pour rejoindre ! Rappel : tapez !play pour lancer la partie !")
 
 	def melange(self,serv):
 		for player in self.players.iterkeys():
@@ -44,7 +47,7 @@ class PeruBot(ircbot.SingleServerIRCBot):
 				if ((e == self.val) or (e == 1)):
 					tmp_nb = tmp_nb+1
 			somme += tmp_nb
-			serv.privmsg("#perudo", player + " révèle " + str(tmp_nb) + " " + str(self.val) + " !")
+			serv.privmsg(self.chan, player + " révèle " + str(tmp_nb) + " " + str(self.val) + " !")
 		return somme
 
         def verif_palifico(self, serv):
@@ -55,12 +58,12 @@ class PeruBot(ircbot.SingleServerIRCBot):
                                 if (e == self.val):
                                         tmp_nb = tmp_nb+1
                         somme += tmp_nb
-                        serv.privmsg("#perudo", player + " révèle " + str(tmp_nb) + " " + str(self.val) + " !")
+                        serv.privmsg(self.chan, player + " révèle " + str(tmp_nb) + " " + str(self.val) + " !")
                 return somme
 
         def verif_elimination(self, serv):
             if len(self.players[self.order[self.curr]]) == 0:
-		serv.privmsg("#perudo", self.order[self.curr] + " est éliminé !")
+		serv.privmsg(self.chan, self.order[self.curr] + " est éliminé !")
 		name = self.order[self.curr]
 		self.order.remove(name)
 		self.players.pop(name, None)
@@ -68,14 +71,14 @@ class PeruBot(ircbot.SingleServerIRCBot):
 
         def verif_gg(self, serv):
             if len(self.players) == 1:
-	        serv.privmsg("#perudo", self.order[0] + " a gagné ! Félicitations !")
+	        serv.privmsg(self.chan, self.order[0] + " a gagné ! Félicitations !")
 		self.reset()
                 return True
             return False
 
         def verif_1de(self, serv):
 	    if len(self.players[self.order[self.curr]]) == 1:
-	        serv.privmsg("#perudo", "PALIFICO !")
+	        serv.privmsg(self.chan, "PALIFICO !")
 	        self.palifico = True
 	    else:
 	        self.palifico = False
@@ -93,7 +96,7 @@ class PeruBot(ircbot.SingleServerIRCBot):
              self.verif_1de(serv)
              self.verif_elimination(serv)
              if (not self.verif_gg(serv)):
-                serv.privmsg("#perudo", "C'est au tour de " + self.order[self.curr] + " !")
+                serv.privmsg(self.chan, "C'est au tour de " + self.order[self.curr] + " !")
                 self.state = 'ENCHERES'
                 self.nb = 0
                 self.val = 0
@@ -101,53 +104,52 @@ class PeruBot(ircbot.SingleServerIRCBot):
 
 	def on_pubmsg(self, serv, ev):
 		author = irclib.nm_to_n(ev.source())
-		chan = ev.target()
 		message = ev.arguments()[0].lower()
 		if message == "!regles":
-			serv.privmsg("#perudo", "Rappel des règles : on doit obligatoirement augmenter le nombre de dés, mais pas forcément la valeur "+\
+			serv.privmsg(self.chan, "Rappel des règles : on doit obligatoirement augmenter le nombre de dés, mais pas forcément la valeur "+\
                                                          "du dé (sauf bien sûr en passant par les 1). 'exact' ne peut être tenté que par le joueur à qui"+\
                                                          " c'est le tour. On ne peut pas avoir plus de 5 dés. En 1 vs 1, 'exact' fait perdre un dé. "+\
                                                          "Si vous n'aimez pas ces règles, pingez 'traklon' pour l'insulter.")
 		elif message == "!comm":
-			serv.privmsg("#perudo", "Annoncer une valeur (ex : 4 fois le dé 6) : 4 6. On ne peut pas écrire paco pour les 1.")
-			serv.privmsg("#perudo", "Annoncer 'exact' : exact OU calza.")
-			serv.privmsg("#perudo", "Confondre un menteur : menteur OU dudo OU faux")
-			serv.privmsg("#perudo", "Meta-jeu : !nbde pour le nombre total de dés. !ordre pour l'ordre du jeu"+\
+			serv.privmsg(self.chan, "Annoncer une valeur (ex : 4 fois le dé 6) : 4 6. On ne peut pas écrire paco pour les 1.")
+			serv.privmsg(self.chan, "Annoncer 'exact' : exact OU calza.")
+			serv.privmsg(self.chan, "Confondre un menteur : menteur OU dudo OU faux")
+			serv.privmsg(self.chan, "Meta-jeu : !nbde pour le nombre total de dés. !ordre pour l'ordre du jeu"+\
 							". !recap pour un récapitulatif du nombre de dés de chacun. !suiv pour avoir le nom du joueur suivant."+\
 							" !quit pour annuler la partie.")
 		elif message == "!nbde":
 			somme = 0
 			for player in self.players.iterkeys():
                                 somme += len(self.players[player])
-			serv.privmsg("#perudo", "Il y a " + str(somme) + " dés en jeu.")
+			serv.privmsg(self.chan, "Il y a " + str(somme) + " dés en jeu.")
 
 		elif message == "!quit":
-                        serv.privmsg("#perudo", "Partie annulée !")
+                        serv.privmsg(self.chan, "Partie annulée !")
 			self.reset()
 
                 elif message == "!recap":
                         for player in self.players.iterkeys():
-                        	serv.privmsg("#perudo", player + " a " + str(len(self.players[player])) + " dés.")
+                        	serv.privmsg(self.chan, player + " a " + str(len(self.players[player])) + " dés.")
 
 		elif message == "!ordre":
-                        serv.privmsg("#perudo", "L'ordre de jeu est : " + ", ".join(self.order))
+                        serv.privmsg(self.chan, "L'ordre de jeu est : " + ", ".join(self.order))
 
 		elif ((message == "!suiv") and (self.state != 'PRE-JEU')):
-                        serv.privmsg("#perudo", "C'est au tour de " + self.order[self.curr] + " !")
+                        serv.privmsg(self.chan, "C'est au tour de " + self.order[self.curr] + " !")
 
 		elif self.state == 'PRE-JEU':
 			if ((message == "!join") and (not (author in self.players))):
-				serv.privmsg("#perudo", author + " a rejoint la partie !")
+				serv.privmsg(self.chan, author + " a rejoint la partie !")
 				self.players[author] = [1,1,1,1,1]
 				self.order.append(author)
 			if ((message == "!play") and (author in self.players)):
 				if (len(self.players) == 1):
-					serv.privmsg("#perudo", "Jouer seul, c'est pas super intéressant...")
+					serv.privmsg(self.chan, "Jouer seul, c'est pas super intéressant...")
 				else:
-					serv.privmsg("#perudo", "La partie débute !")
-					serv.privmsg("#perudo", "Tapez !regles pour connaître les règles en vigueur.")
-					serv.privmsg("#perudo", "Tapez !comm pour connaîtres les commandes (normalement assez intuitives).")
-					serv.privmsg("#perudo", "L'ordre de jeu est : "+ ", ".join(self.order))
+					serv.privmsg(self.chan, "La partie débute !")
+					serv.privmsg(self.chan, "Tapez !regles pour connaître les règles en vigueur.")
+					serv.privmsg(self.chan, "Tapez !comm pour connaîtres les commandes (normalement assez intuitives).")
+					serv.privmsg(self.chan, "L'ordre de jeu est : "+ ", ".join(self.order))
 					self.state = 'ENCHERES'
 					self.melange(serv)
 
@@ -164,18 +166,18 @@ class PeruBot(ircbot.SingleServerIRCBot):
                                             self.nb = tmp_nb
                                             self.val = tmp_val
                                 	    self.curr = (self.curr+1)%(len(self.players))
-					    serv.privmsg("#perudo", "C'est au tour de " + self.order[self.curr] + " !")
+					    serv.privmsg(self.chan, "C'est au tour de " + self.order[self.curr] + " !")
                                         else:
-                                            serv.privmsg("#perudo", "Enchère erronée !")
+                                            serv.privmsg(self.chan, "Enchère erronée !")
 				elif (((message == 'faux') or (message == 'menteur') or (message == 'dudo')) and self.val > 0):
 					self.state = 'FAUX'
 				elif (((message == 'exact') or (message == 'calza')) and self.val > 0):
 					self.state = 'EXACT'
 				elif re.match (r'[0-9]+ [0-9]', message):
-					serv.privmsg("#perudo", "Crétin.")
+					serv.privmsg(self.chan, "Crétin.")
 			else:
 				if (re.match (r'[1-9][0-9]* [1-6]', message) or (message == 'exact') or (message == 'menteur') or (message == 'dudo') or (message == 'faux') or (message == 'calza')):
-					serv.privmsg("#perudo", author + " : ce n'est pas ton tour, mais celui de " + str(self.order[self.curr]) + " !")
+					serv.privmsg(self.chan, author + " : ce n'est pas ton tour, mais celui de " + str(self.order[self.curr]) + " !")
 
 
 		if self.state == 'FAUX':
@@ -185,10 +187,10 @@ class PeruBot(ircbot.SingleServerIRCBot):
                 	somme = self.verif(serv)
 
                     if (self.nb <= somme):
-		        serv.privmsg("#perudo", "Avec " + str(somme) + " " + str(self.val) + ", l'enchère était correcte ! " + author + " perd un dé !")
+		        serv.privmsg(self.chan, "Avec " + str(somme) + " " + str(self.val) + ", l'enchère était correcte ! " + author + " perd un dé !")
 	            else:
 		        self.curr = (self.curr+len(self.players)-1)%(len(self.players))
-			serv.privmsg("#perudo", "Avec " + str(somme) + " " + str(self.val) + ", l'enchère était fausse ! " + self.order[self.curr] + " perd un dé !")
+			serv.privmsg(self.chan, "Avec " + str(somme) + " " + str(self.val) + ", l'enchère était fausse ! " + self.order[self.curr] + " perd un dé !")
 
 		    self.players[self.order[self.curr]] = self.players[self.order[self.curr]][1:]
 
@@ -203,16 +205,16 @@ class PeruBot(ircbot.SingleServerIRCBot):
 
                         if (self.nb == somme):
 				if (len(self.players) > 2):
-                                	serv.privmsg("#perudo", "Avec " + str(somme) + " " + str(self.val) + ", l'enchère était exacte ! " + author + " gagne un dé !")
+                                	serv.privmsg(self.chan, "Avec " + str(somme) + " " + str(self.val) + ", l'enchère était exacte ! " + author + " gagne un dé !")
 					if len(self.players[author]) < 5:
 						(self.players[author]).append(1)
 
 				else:
         	                        self.curr = (self.curr+len(self.players)-1)%(len(self.players))
-	                                serv.privmsg("#perudo", "Avec " + str(somme) + " " + str(self.val) + ", l'enchère était exacte ! " + self.order[self.curr] + " perd un dé !")
+	                                serv.privmsg(self.chan, "Avec " + str(somme) + " " + str(self.val) + ", l'enchère était exacte ! " + self.order[self.curr] + " perd un dé !")
 					self.players[self.order[self.curr]] = self.players[self.order[self.curr]][1:]
                         else:
-				serv.privmsg("#perudo", "Avec " + str(somme) + " " + str(self.val) + ", l'enchère était inexacte ! " + author + " perd un dé !")
+				serv.privmsg(self.chan, "Avec " + str(somme) + " " + str(self.val) + ", l'enchère était inexacte ! " + author + " perd un dé !")
                	        	self.players[self.order[self.curr]] = self.players[self.order[self.curr]][1:]
 
                         self.nouv_tirage(serv)
